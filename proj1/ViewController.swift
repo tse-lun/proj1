8e29b3bfb9754695ec55c2bd2d5a1ead09e7388e
tse-lun <tse_lun@hotmail.com>
2025-03-27 16:14:08 +0800

    part 2 complete

--- proj1/ViewController.swift
+++ proj1/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var pictures = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        print("test commit")
+ 
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        
+        print(pictures)
     }
+    
+    
+    
 
 
 }
