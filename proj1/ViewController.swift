14ea9ea21191877c4699bc150a2cfea0b7d7db87
tse-lun <tse_lun@hotmail.com>
2025-03-27 15:47:58 +0800

    Initial Commit

---
+++ proj1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  proj1
+//
+//  Created by Stephen Chiu on 2025-03-27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
